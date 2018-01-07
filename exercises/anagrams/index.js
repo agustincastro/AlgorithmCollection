37e2e84196a36b64ce74a5e21ef67d28c9d1aa1f
Agustin Castro <agustin.castro.91@gmail.com>
2018-01-07 02:01:24 -0300

    Added chunk and anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+    const cleanString = str => str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join();
+
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+
+function anagrams2(stringA, stringB) {
+
+    const createCharMap = str => {
+        const charMap = {};
+        const cleanStr = str.replace(/[^\w]/g, '').toLowerCase();
+        for (let char of cleanStr) {
+            charMap[char] = charMap[char] + 1 || 1;
+        }
+        return charMap;
+    };
+
+    const mapA = createCharMap(stringA);
+    const mapB = createCharMap(stringB);
+
+    if (Object.keys(mapA).length !== Object.keys(mapB).length) {
+        return false;
+    } else {
+        for (let key in mapA) {
+            if (mapA[key] !== mapB[key]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const result = [];
+    let index = 0;
+    
+    while (index < array.length) {
+        result.push(array.slice(index, index + size));
+        index += size;
+    }
+    return result;
+}
+
+function chunkModifiesInput(array, size) {
+    const result = [];
+    while (array.length > 0) {
+        result.push(array.splice(0, size));
+    }
+    return result;
+}
+
+function chunk2(array, size) {
+    const result = [];
+
+    for (let element of array) {
+        const lastElement = result[result.length - 1];
+        if (!lastElement || lastElement.length === size) {
+            result.push([element]);
+        } else {
+            lastElement.push(element);
+        }
+    }
+    return result;
+}
+
+chunk([1, 2, 3, 4, 5, 6, 7], 2)
 
 module.exports = chunk;
