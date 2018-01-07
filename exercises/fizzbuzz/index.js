c4f03a45781f220cf50d1cda320b0edcb1e0762e
Agustin Castro <agustin.castro.91@gmail.com>
2018-01-07 00:18:09 -0300

    Added some fizzbuzz and maxchar

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) console.log('fizzbuzz');
+        else if (i % 3 === 0) console.log('fizz');
+        else if (i % 5 === 0) console.log('buzz');
+        else console.log(i);
+    }
+}
+
+function fizzBuzz2(n) {
+    for (let i = 1; i <= n; i++) {
+        let print = '';
+        print = i % 3 == 0 ? print + "fizz" : print;
+        print = i % 5 == 0 ? print + "buzz" : print;
+        if (print == '') console.log(i);
+        else console.log(print);
+    }
+}
 
 module.exports = fizzBuzz;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    for(let char of str){
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+    let maxCount = 0;
+    let maxChar = "";
+    for(let key in charMap){
+        if(maxCount < charMap[key]){
+            maxCount = charMap[key];
+            maxChar = key;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
