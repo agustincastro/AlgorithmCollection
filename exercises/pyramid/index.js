54ea815f7ec3b1fc69d140fccb5046dadac97c92
Agustin Castro <agustin.castro.91@gmail.com>
2018-01-20 18:05:00 -0300

    Added pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,40 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+    if (row === n) {
+        return;
+    }
+    const pyramidLength = 2 * n - 1;
+    if (level.length === pyramidLength) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+    let char = '';
+    const midPoint = Math.floor(pyramidLength / 2);
+    if (midPoint - row <= level.length && midPoint + row >= level.length) {
+        char = '#';
+    } else {
+        char = ' ';
+    }
+    pyramid(n, row, level + char);
+}
+
+function pyramid2(n) {
+    const pyramidLength = (2 * n) - 1;
+    const midPoint = Math.floor(pyramidLength / 2);
+
+    for (let row = 0; row < n; row++) {
+        let level = '';
+        for (let column = 0; column < pyramidLength; column++) {
+            if (midPoint - row <= column && midPoint + row >= column) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+}
 
 module.exports = pyramid;
