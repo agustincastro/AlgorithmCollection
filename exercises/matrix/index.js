18f1856a086a84cd2eaaf0c9b53bbef2276e437c
Agustin Castro <agustin.castro.91@gmail.com>
2018-01-27 18:08:06 -0300

    added matrix solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,50 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+
+    const initMatrix = (size) => {
+        var arr = new Array(size);
+        for (var i = 0; i < size; i++) {
+            arr[i] = new Array(size);
+        }
+        return arr;
+    }
+
+    const spiral = initMatrix(n);
+    let left = 0,
+        right = n - 1,
+        top = 0,
+        bottom = n - 1,
+        counter = 1;
+
+    while (top <= bottom && left <= right) {
+        // right ->
+        for (let i = left; i <= right; i++) {
+            spiral[top][i] = counter;
+            counter++;
+        }
+        top++;
+        // Down |
+        for (let i = top; i <= bottom; i++) {
+            spiral[i][right] = counter;
+            counter++;
+        }
+        right--;
+        // Left <-
+        for (let i = right; i >= left; i--) {
+            spiral[bottom][i] = counter;
+            counter++;
+        }
+        bottom--;
+        // Up |
+        for (let i = bottom; i >= top; i--) {
+            spiral[i][left] = counter;
+            counter++;
+        }
+        left++;
+    }
+    return spiral;
+}
 
 module.exports = matrix;
